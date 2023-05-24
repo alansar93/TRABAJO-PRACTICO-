1f352d746d93ce6738ba580661406f158e367cd7
Alan Sarchman <alansarchman@live.com.ar>
2023-05-24 17:24:47 -0300

    realizado algo de maquetado primer producto, faltan 9 y funciones

--- algoritmo.js
+++ algoritmo.js
@@ -1,18 +1,25 @@
 const cantidadMostrar = document.querySelector("#cantidad-prodcutos-mostrar")
 const cantidadPermitidaComprar = document.querySelector("#cantidad-permitida-producto")
 const coloresUsuario = document.querySelector("#colores")
-
+const MostrarProducto = document.querySelector("#producto-muestra")
+const cadaProductoImg = document.querySelector("fieldset") 
+const cantProdElegido = document.querySelector("#cantidad-producto-elegida")
 
 const pParrafoInfo = document.querySelector("#detalle-usuario-eleccion")
+
+
 
 
 function opcionesUsuario() {
     let cantidadElegidaUsuario = cantidadMostrar.value
     let cantidadPermitidaElegidaUsuario = cantidadPermitidaComprar.value
     let coloresElegidoUsuario = coloresUsuario.value
+    
 
     pParrafoInfo.innerHTML = "La cantidad de prodcutos a mostrar es: " + cantidadElegidaUsuario + "<br>" +
         "La cantidad de prodcutos permitidos por compra es: " + cantidadPermitidaElegidaUsuario + "<br>" +
         "Los colores selecciones a usar son: " + coloresElegidoUsuario
  
+    cantidadPermitidaComprar.innerHTML = cantidadElegidaUsuario
+    
     }
