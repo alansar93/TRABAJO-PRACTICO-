9a041d1739de2329923ecc5eece4d6eaaebf61c5
Alan Sarchman <alansarchman@live.com.ar>
2023-06-02 18:45:59 -0300

    falta poner productos que elige el usuario

--- algoritmo.js
+++ algoritmo.js
@@ -2,32 +2,34 @@ const cantidadMostrar = document.querySelector("#cantidad-prodcutos-mostrar")
 const cantidadPermitidaComprar = document.querySelector("#cantidad-permitida-producto")
 const coloresUsuario = document.querySelector("#colores")
 const MostrarProducto = document.querySelector("#producto-muestra")
-const cadaProductoImg = document.querySelector("fieldset") 
+const cadaProductoImg = document.querySelector("fieldset")
 const cantProdElegido = document.querySelector("#cantidad-producto-elegida")
 
 const pParrafoInfo = document.querySelector("#detalle-usuario-eleccion")
 
+let imgProducto2 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQECuaelhbHoT-NQhAPAX63MEQ2gYEGsm90eQ&usqp=CAU"
 
 
 //* Esta funcion, pone el texto que elige el usuario*//
 function opcionesUsuario() {
-    let cantidadElegidaUsuario = cantidadMostrar.value
-    let cantidadPermitidaElegidaUsuario = cantidadPermitidaComprar.value
-    let coloresElegidoUsuario = coloresUsuario.value
-      
-    
-    pParrafoInfo.innerHTML = "La cantidad de prodcutos a mostrar es: " + cantidadElegidaUsuario + "<br>" +
-    "La cantidad de prodcutos permitidos por compra es: " + cantidadPermitidaElegidaUsuario + "<br>" +
-        "Los colores selecciones a usar son: " + coloresElegidoUsuario
+   let cantidadElegidaUsuario = cantidadMostrar.value
+   let coloresElegidoUsuario = coloresUsuario.value
+   let cantidadPermitidaElegidaUsuario = cantidadPermitidaComprar.value
+
+
+   pParrafoInfo.innerHTML = "La cantidad de prodcutos a mostrar es: " + cantidadElegidaUsuario + "<br>" +
+      "La cantidad de prodcutos permitidos por compra es: " + cantidadPermitidaElegidaUsuario + "<br>" +
+      "Los colores selecciones a usar son: " + coloresElegidoUsuario
+
+   for (let i = 1; i <= cantidadPermitidaElegidaUsuario; i++) {
+      cantProdElegido.innerHTML += `<option value="${i}"> ${i} </option>`
+   }
  
-     for (let i = 1; i <= cantidadPermitidaElegidaUsuario; i++) {
-        cantProdElegido.innerHTML += `<option value="${i}"> ${i} </option>`
+  
 
-     }
-    
-    }
+}
 
 
 
-         
-    
+
+
